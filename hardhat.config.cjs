b61d6818a5100f175829fa0e48aafc983155a621
Quang-Dieu Nguyen <86088186+itzmealvin@users.noreply.github.com>
2024-05-30 12:23:46 +0700

    build: add Hardhat build config

---
+++ hardhat.config.cjs
@@ -0,0 +1,15 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.20",
+  settings: {
+    optimizer: {
+      enabled: true,
+      runs: 200,
+    },
+  },
+  paths: {
+    sources: "./contracts",
+    artifacts: "./src/compiled",
+    cache: "./src/compiled",
+  },
+};
