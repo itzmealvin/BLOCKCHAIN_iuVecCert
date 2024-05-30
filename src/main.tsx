e3b41449a10c41cec398113b01a59760c04e22fb
Quang-Dieu Nguyen <86088186+itzmealvin@users.noreply.github.com>
2024-05-30 12:37:03 +0700

    feat: main React source code

---
+++ src/main.tsx
@@ -0,0 +1,33 @@
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import router from "./components/route";
+import "./index.css";
+import theme from "./theme";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnMount: false,
+      refetchOnReconnect: false,
+      retry: false, // Do not auto retry
+      cacheTime: 1000 * 60 * 60 * 24, // Cache up-to 1 day
+    },
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+    </ChakraProvider>
+  </React.StrictMode>
+);
