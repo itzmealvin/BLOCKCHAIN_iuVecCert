1b0846e06641afa1e16d9f1688cd3bec4aa08e9a
Quang-Dieu Nguyen <86088186+itzmealvin@users.noreply.github.com>
2024-05-30 12:32:06 +0700

    feat: add main proof generation worker

---
+++ server/workers/ProofWorker.ts
@@ -0,0 +1,32 @@
+import { parentPort, workerData } from "worker_threads";
+import { FileParamsDto } from "../dtos/FileParamsDto";
+import { genProof, genVerifierContractParams } from "../libs/lib-kzg";
+
+function genProofs(
+  coeffs: bigint[],
+  chunks: FileParamsDto[],
+  commit: bigint[]
+) {
+  const results: FileParamsDto[] = [];
+  for (let i = 0; i < chunks.length; i++) {
+    const { fileIndex, fileHash } = chunks[i];
+    const proof = genProof(coeffs, parseInt(fileIndex));
+    const params = genVerifierContractParams(
+      commit,
+      proof,
+      parseInt(fileIndex),
+      BigInt(fileHash)
+    );
+    results.push({
+      commitment: params.commitment,
+      fileProof: params.proof,
+      fileIndex: params.index,
+      fileHash: params.value,
+    });
+  }
+  return results;
+}
+
+parentPort!.postMessage(
+  genProofs(workerData.coeffs, workerData.chunks, workerData.commit)
+);
