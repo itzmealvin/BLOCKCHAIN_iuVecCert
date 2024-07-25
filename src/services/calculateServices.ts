68807c0d68a02648f8dcae03c88f8328d09775a4
Quang-Dieu Nguyen <86088186+itzmealvin@users.noreply.github.com>
2024-07-25 14:10:46 +0700

    fix: update dto to match server

--- src/services/calculateServices.ts
+++ src/services/calculateServices.ts
@@ -1,7 +1,10 @@
 import APIClient from "./apiClient";
 
+import { FileProps } from "./FilesServices";
+
 export interface StringValues {
   values: string[];
+  challenge?: FileProps;
 }
 
 export const coefficientsService = new APIClient<StringValues>("/coefficients");
