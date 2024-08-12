97504b38fcdd0fd85ee9a430185f532792c161ee
Quang-Dieu Nguyen <86088186+itzmealvin@users.noreply.github.com>
2024-08-12 20:34:43 +0700

    refactor: renaming worker files

--- server/workers/worker.js
+++
@@ -1,4 +0,0 @@
-import { resolve } from "path";
-import { workerData } from "worker_threads";
-
-require(resolve(__dirname, workerData.path));
---
+++ server/workers/workerWrapperOld.js
@@ -0,0 +1,4 @@
+import {resolve} from "path";
+import {workerData} from "worker_threads";
+
+require(resolve(__dirname, workerData.path));
