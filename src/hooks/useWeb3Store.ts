edde2bce20a03a4f7b2d2d91759e1d237a190e97
Quang-Dieu Nguyen <86088186+itzmealvin@users.noreply.github.com>
2024-08-15 23:25:30 +0700

    feat: add new field for web3 hook

--- src/hooks/useWeb3Store.ts
+++ src/hooks/useWeb3Store.ts
@@ -2,22 +2,24 @@ import {mountStoreDevtool} from "simple-zustand-devtools";
 import {create} from "zustand";
 
 interface Web3AuthStates {
+    issuerAddress: string;
     contractAddress: string;
-
 }
 
 interface Web3AuthActions {
+    setIssuerAddress: (address: string) => void;
     setContractAddress: (address: string) => void;
     resetAddress: () => void;
 }
 
 const initialState: Web3AuthStates = {
+    issuerAddress: "",
     contractAddress: "",
 };
 
 const useWeb3Store = create<Web3AuthStates & Web3AuthActions>((set) => ({
     ...initialState,
-
+    setIssuerAddress: (address) => set(() => ({issuerAddress: address})),
     setContractAddress: (address) => set(() => ({contractAddress: address})),
     resetAddress: () => set(initialState),
 }));
